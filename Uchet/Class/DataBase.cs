e5f3c353124017cf1b80cb210fb18a8ab8320910
JavisNet <Pkpersonal123567@mail.ru>
2022-12-20 20:37:41 +0700

    start

---
+++ Uchet/Class/DataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uchet.Class
+{
+    internal class DataBase
+    {
+        public static string connStr = @"";
+    }
+}
